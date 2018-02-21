86dd54aaf4cc66342a2732c9c1957e58a112a28b
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2018-02-22 00:09:46 +0900

    いろいろとついか

--- app/src/main/java/zukkey/tubers/MainActivity.kt
+++ app/src/main/java/zukkey/tubers/MainActivity.kt
@@ -1,34 +1,30 @@
 package zukkey.tubers
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import zukkey.tubers.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
-
-  private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-    when (item.itemId) {
-      R.id.navigation_home -> {
-        message.setText(R.string.title_home)
-        return@OnNavigationItemSelectedListener true
-      }
-      R.id.navigation_dashboard -> {
-        message.setText(R.string.title_dashboard)
-        return@OnNavigationItemSelectedListener true
-      }
-      R.id.navigation_notifications -> {
-        message.setText(R.string.title_notifications)
-        return@OnNavigationItemSelectedListener true
-      }
-    }
-    false
-  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-
-    navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    binding.navigation.navigation.setOnNavigationItemSelectedListener { item ->
+      when (item.itemId) {
+        R.id.navigation_home -> {
+          message.setText(R.string.title_home)
+        }
+        R.id.navigation_dashboard -> {
+          message.setText(R.string.title_dashboard)
+        }
+        R.id.navigation_notifications -> {
+          message.setText(R.string.title_notifications)
+        }
+      }
+      false
+    }
   }
 }
